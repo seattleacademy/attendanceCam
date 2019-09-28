62423a8e3814fbfec9b430b96221b3e15a298801
Gary Anderson <ganderson@seattleacademy.org>
2019-09-28 05:15:51 -0700

    faceMaker starer code

---
+++ php/faces.php
@@ -0,0 +1,19 @@
+<?php
+$dir = "/var/www/faces/"; //global directory
+$dir = "../faces/"; // local directory, use chmod 777 faces to give write access to the server
+$return_array = array();
+
+if(is_dir($dir)){
+    if($dh = opendir($dir)){
+        while(($file = readdir($dh)) != false){
+            if($file == "." or $file == ".."){
+            } else {
+                $return_array[] = $file; // Add the file to the array
+            }
+        }
+    }
+
+    echo json_encode($return_array);
+}
+
+?>
