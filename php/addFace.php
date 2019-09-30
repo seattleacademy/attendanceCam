7cf2aac6ecd9aa774644942ed06cd6638e6c9824
Gary Anderson <ganderson@seattleacademy.org>
2019-09-29 18:35:47 -0700

    Work with webcam

--- php/addFace.php
+++ php/addFace.php
@@ -1,12 +1,33 @@
 <?php
 $upload_dir = "../faces/";
-$img = $_POST['hidden_data'];
+$img = $_POST['img'];
 $person = $_POST['person'];
-$img = str_replace('data:image/jpeg;base64,', '', $img);
+if(substr( $img, 11, 4 ) === "jpeg"){
+	$img = str_replace('data:image/jpeg;base64,', '', $img);
+	$imageFileType = 'jpeg';
+}
+else{
+	$img = str_replace('data:image/png;base64,', '', $img);
+	$imageFileType = 'png';
+}
 $img = str_replace(' ', '+', $img);
 $data = base64_decode($img);
+$imgHash = md5($data);
 $counter = 1;
-$imageFileType = 'jpeg';
+
+$files = scandir($upload_dir);
+
+unset($files[array_search('.',$files)],$files[array_search('..',$files)]);
+
+    foreach ($files as $file) {
+    $fileHash = md5_file($upload_dir.$file);
+	if($fileHash == $imgHash){
+		print($file);
+		unlink($upload_dir.$file);
+	}
+    
+
+    }
 
 while(file_exists($upload_dir . $person . "." . $counter . "."  . $imageFileType)) {
     $counter++;
@@ -14,8 +35,9 @@ while(file_exists($upload_dir . $person . "." . $counter . "."  . $imageFileType
 
 $file = $upload_dir . $person . "." . $counter . "."  . $imageFileType;
 $success = file_put_contents($file, $data);
-print $success ? $file : 'Unable to save the file.';
-  header( "refresh:1; url=../faces/" ); 
 
 
+
+print(md5($data));
+//print $success ? $file : 'Unable to save the file.';
 ?>
